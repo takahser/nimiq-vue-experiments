8547d340d9fddc86401fe0eb11e25f72e3068840
S E R A Y A <seraya.takahashi@outlook.com>
2018-11-16 12:18:29 +0100

    added correct Nimiq import and tried to initialize Nimiq

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import Vue from 'vue';
 import App from './App.vue';
-import * as Nimiq from './nimiq.js';
+
+// tslint:disable-next-line:max-line-length
+// import * as Nimiq from './nimiq';  => Nimiq will equal to: Module {__esModule: true, Symbol(Symbol.toStringTag): "Module"}
+// import Nimiq from './nimiq';       => Nimiq will  equal to: undefined
+// import { Nimiq } from './nimiq';   => Nimiq will  equal to: undefined
+
+import * as Nimiq from './nimiq';
 
 Vue.config.productionTip = false;
 
@@ -9,4 +15,50 @@ new Vue({
 }).$mount('#app');
 
 // tslint:disable-next-line:no-console
-console.log(Nimiq);
+console.log('Nimiq module is available: ', Nimiq);
+
+Nimiq.init(
+  async () => {
+
+    // avoid error: 'GenesisConfig already initialized'
+    if (Nimiq.GenesisConfig._config) {
+      // tslint:disable-next-line:no-console
+      console.log('Nimiq GenesisConfig is already loaded, using that one...');
+    } else {
+      // tslint:disable-next-line:no-console
+      console.log('Loading Nimiq GenesisConfig...');
+      Nimiq.GenesisConfig.main();
+    }
+
+    this.nimInstance.consensus = await Nimiq.Consensus.nano();
+
+    this.nimInstance.consensus.on('established', async () => {
+      this.nimConsensusEstablished = true;
+      // tslint:disable-next-line:no-console
+      console.log('Nimiq Consensus established 🎉');
+    });
+
+    this.nimInstance.blockchain = this.nimInstance.consensus.blockchain;
+    this.nimInstance.mempool = this.nimInstance.consensus.mempool;
+    this.nimInstance.network = this.nimInstance.consensus.network;
+    this.nimInstance.accounts = this.nimInstance.blockchain.accounts;
+
+    this.nimInstance.network.connect();
+  },
+  (code) => {
+    switch (code) {
+      case Nimiq.ERR_WAIT:
+        alert('Another Nimiq instance is already running');
+        break;
+      case Nimiq.ERR_UNSUPPORTED:
+        alert('Browser not supported');
+        break;
+      case Nimiq.Wallet.ERR_INVALID_WALLET_SEED:
+        alert('Invalid wallet seed');
+        break;
+      default:
+        alert('Nimiq initialization error');
+        break;
+    }
+  },
+);
