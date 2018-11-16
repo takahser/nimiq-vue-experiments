f0d818f0650accbe0e334d8d2ffacf1b9bea4b3e
S E R A Y A <seraya.takahashi@outlook.com>
2018-11-16 01:34:16 +0100

    added nimiq

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import Vue from 'vue';
 import App from './App.vue';
+import * as Nimiq from './nimiq.js';
 
 Vue.config.productionTip = false;
 
 new Vue({
   render: (h) => h(App),
 }).$mount('#app');
+
+// tslint:disable-next-line:no-console
+console.log(Nimiq);
